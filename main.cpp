622ec77ac9793e54f27e3e7585a8a48225143646
Alessandro Croci <alessandro.croci@iit.it>
2024-05-03 17:04:37 +0200

    Add main.cpp and CMakeLists.txt

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <gz/sim/EntityComponentManager.hh>
+#include <gz/sim/EventManager.hh>
+#include <iostream>
+
+#include <BipedalLocomotion/RobotInterface/YarpRobotControl.h>
+#include <BipedalLocomotion/RobotInterface/YarpSensorBridge.h>
+
+#include <gzyarp/DeviceRegistry.hh>
+
+#include <gz/common/Console.hh>
+#include <gz/sim/Entity.hh>
+#include <gz/sim/Joint.hh>
+#include <gz/sim/Model.hh>
+#include <gz/sim/TestFixture.hh>
+#include <gz/sim/World.hh>
+
+#include <filesystem>
+#include <memory>
+#include <sdf/Element.hh>
+#include <yarp/dev/PolyDriverList.h>
+
+int main()
+{
+
+    return 0;
+}
